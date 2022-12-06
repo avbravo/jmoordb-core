2dc70a0ffc6c2416725041e3deefd12967164d96
avbravo <avbravo@avbravo>
2022-12-06 12:44:33 -0500

    0.4.200

--- src/main/java/com/jmoordb/core/repository/CrudRepository.java
+++ src/main/java/com/jmoordb/core/repository/CrudRepository.java
@@ -3,14 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.jmoordb.core.repository;
-
-import com.jmoordb.core.annotation.repository.Count;
+import com.jmoordb.core.annotation.repository.CountAll;
 import com.jmoordb.core.annotation.repository.DeleteBy;
 import com.jmoordb.core.annotation.repository.Find;
 import com.jmoordb.core.annotation.repository.Save;
 import com.jmoordb.core.annotation.repository.Update;
 import com.jmoordb.core.model.Pagination;
-import com.jmoordb.core.model.Search;
 import com.jmoordb.core.model.Sorted;
 import java.util.List;
 import java.util.Optional;
@@ -45,6 +43,6 @@ public interface CrudRepository<T, PK> {
     @DeleteBy
     public Long deleteByPk(PK id);
 
-    @Count
-    public Long count(Search...search);
+    @CountAll
+    public Long count();
 }
